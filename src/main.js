e9a066ed0503c83cf66c00df087f1c8a2f463d31
储笑 <shmilyclxy@163.com>
2017-08-07 18:02:13 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,20 +1,20 @@
-import Vue from 'vue'
-import App from './App'
-import player from './player/vue-player.js'
-
-Vue.config.productionTip = false
-
-Vue.use(player)
-
-new Vue({
-  el: '#app',
-  template: '<App/>',
-  components: { App }
-})
-
-// import player from './player/vue-player.vue'
+// import Vue from 'vue'
+// import App from './App'
+// import player from './player/vue-player.js'
 //
-// const playerPlugin = {}
-// export default playerPlugin.install = (Vue, options) => {
-// 	Vue.component('vue-player', player)
-// }
+// Vue.config.productionTip = false
+//
+// Vue.use(player)
+//
+// new Vue({
+//   el: '#app',
+//   template: '<App/>',
+//   components: { App }
+// })
+
+import player from './player/vue-player.vue'
+
+const playerPlugin = {}
+export default playerPlugin.install = (Vue, options) => {
+	Vue.component('vue-player', player)
+}
